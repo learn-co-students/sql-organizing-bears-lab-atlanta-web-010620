6a54f362891cc1bd5b852bc10d1c1212b8b9e6df
Rashad Dixon <dixoncodes@gmail.com>
2020-01-15 15:38:42 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bears (
+  id INTEGER Primary Key,
+  name text,
+  age int, 
+  gender text,
+  color text,
+  temperament text,
+  alive boolean
+);
+
+
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,45 @@
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age from bears
+  WHERE gender = 'F'"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  "Select name from bears
+  ORDER BY name"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  "Select name, age from bears
+  Where alive = true
+  ORDER BY age ASC"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "Select name, age from bears
+  ORDER BY age DESC
+  LIMIT 1"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "Select name, age from bears
+  ORDER BY age ASC
+  LIMIT 1"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  "Select color, Count(color) from bears
+  group by color
+  order by color DESC
+  limit 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "Select Count(temperament) from bears
+  where temperament = 'goofy'
+  group by temperament"
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  "Select * from bears
+  where name IS NULL"
 end
