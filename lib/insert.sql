66664eb0beeed3874283b75521343a2a50caefd6
tylergreason <tyler.greason@protonmail.com>
2020-01-15 09:51:06 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate", 1, "M", "brown", "angry",  true); 
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Rowdy", 1, "F", "brown", "angry",  true); 
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Tabitha", 1, "M", "brown", "angry",  true); 
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Segeant Brown", 1, "M", "brown", "angry",  true); 
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Melissa", 1, "F", "brown", "angry",  true); 
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Grinch", 1, "M", "brown", "angry",  true); 
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Wendy", 1, "F", "brown", "angry",  true); 
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES(null, 1, "M", "brown", "angry",  true ); 
